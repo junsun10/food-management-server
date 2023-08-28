6fbeac314f3cc704fe6b2d551a66efd218206616
Junsun Lee <junsun1014@naver.com>
2023-08-28 18:35:21 +0900

    feat: define models
    #1

--- FoodManagementAPI/admin.py
+++ FoodManagementAPI/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from .models import IngredientCategory, Ingredient, UserIngredient, RecipeCategory, Recipe, RecipeIngredient, Cart
+
+
+admin.site.register(IngredientCategory)
+admin.site.register(Ingredient)
+admin.site.register(UserIngredient)
+admin.site.register(RecipeCategory)
+admin.site.register(Recipe)
+admin.site.register(RecipeIngredient)
+admin.site.register(Cart)
--- FoodManagementAPI/models.py
+++ FoodManagementAPI/models.py
@@ -1,1 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+
+class IngredientCategory(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+
+class Ingredient(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    category = models.ForeignKey(IngredientCategory, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+
+class UserIngredient(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=5, decimal_places=1)
+
+    def __str__(self):
+        return self.user.username + "_" + self.ingredient.title + "_" + str(self.quantity)
+
+
+class RecipeCategory(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    code = models.IntegerField()
+    category = models.ForeignKey(RecipeCategory, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.recipe.title + "_" + self.ingredient.title
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    buy = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('user', 'ingredient')
+
+    def __str__(self):
+        return self.user.username + "_" + self.ingredient.title
